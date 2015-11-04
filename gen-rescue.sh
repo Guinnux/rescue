7b652da3065f215cf361ab8e8dec21a7dc15970f
John Eigelaar <john@kses.net>
2015-11-04 16:56:21 +0200

    Added the last bits in order to generate the rescue.

--- gen-rescue.sh
+++ gen-rescue.sh
@@ -2,49 +2,56 @@
 IMG=$PWD/image
 mkdir -p $PWD/image/var/lib/pacman
 #arm-gnx5-linux-gnueabi-pacman -r $PWD/image -Sy 
-arm-gnx5-linux-gnueabi-pacman -r $PWD/image -S filesystem 
+arm-gnx5-linux-gnueabi-pacman -r $PWD/image -S --noconfirm filesystem 
 
-arm-gnx5-linux-gnueabi-pacman -r $PWD/image -U $PWD/repo/busybox-rescue-1.21.1-1-arm.pkg.tar.xz
-arm-gnx5-linux-gnueabi-pacman -r $PWD/image -U $PWD/repo/libarchive-rescue-3.1.2-8-arm.pkg.tar.xz
-arm-gnx5-linux-gnueabi-pacman -r $PWD/image -U $PWD/repo/pacman-rescue-4.2.1-2-arm.pkg.tar.xz
-arm-gnx5-linux-gnueabi-pacman -r $PWD/image -U $PWD/repo/init-rescue-1.0.0-1-any.pkg.tar.xz
-arm-gnx5-linux-gnueabi-pacman -r $PWD/image -U $PWD/repo/openssl-rescue-1.0.2.d-1-arm.pkg.tar.xz
-arm-gnx5-linux-gnueabi-pacman -r $PWD/image -U $PWD/repo/e2fsprogs-rescue-1.42.13-1-arm.pkg.tar.xz
+#Remove this one as busybox brings its own that works with ash
+rm -fR $IMG/etc/profile
 
-arm-gnx5-linux-gnueabi-pacman -r $PWD/image -S iproute2
-arm-gnx5-linux-gnueabi-pacman -r $PWD/image -S gcc-libs
-arm-gnx5-linux-gnueabi-pacman -r $PWD/image -S acl
-arm-gnx5-linux-gnueabi-pacman -r $PWD/image -S expat
-arm-gnx5-linux-gnueabi-pacman -r $PWD/image -S lzo
-arm-gnx5-linux-gnueabi-pacman -r $PWD/image -S bzip2
-arm-gnx5-linux-gnueabi-pacman -r $PWD/image -S dropbear
+arm-gnx5-linux-gnueabi-pacman -r $PWD/image -U --noconfirm $PWD/repo/busybox-rescue-1.21.1-1-arm.pkg.tar.xz
+arm-gnx5-linux-gnueabi-pacman -r $PWD/image -U --noconfirm $PWD/repo/libarchive-rescue-3.1.2-8-arm.pkg.tar.xz
+arm-gnx5-linux-gnueabi-pacman -r $PWD/image -U --noconfirm $PWD/repo/pacman-rescue-4.2.1-2-arm.pkg.tar.xz
+arm-gnx5-linux-gnueabi-pacman -r $PWD/image -U --noconfirm $PWD/repo/init-rescue-1.0.0-1-any.pkg.tar.xz
+arm-gnx5-linux-gnueabi-pacman -r $PWD/image -U --noconfirm $PWD/repo/openssl-rescue-1.0.2.d-1-arm.pkg.tar.xz
+arm-gnx5-linux-gnueabi-pacman -r $PWD/image -U --noconfirm $PWD/repo/e2fsprogs-rescue-1.42.13-1-arm.pkg.tar.xz
+arm-gnx5-linux-gnueabi-pacman -r $PWD/image -U --noconfirm $PWD/repo/log4cpp-1.1.1-1-arm.pkg.tar.xz
+arm-gnx5-linux-gnueabi-pacman -r $PWD/image -U --noconfirm $PWD/repo/libklog-1.1-1-arm.pkg.tar.xz
+arm-gnx5-linux-gnueabi-pacman -r $PWD/image -U --noconfirm $PWD/repo/gnxid-2.0.2-1-arm.pkg.tar.xz
+arm-gnx5-linux-gnueabi-pacman -r $PWD/image -U --noconfirm $PWD/repo/gnx-installer-3.0-1-any.pkg.tar.xz
 
-#exit 0
+arm-gnx5-linux-gnueabi-pacman -r $PWD/image -S --noconfirm iproute2
+arm-gnx5-linux-gnueabi-pacman -r $PWD/image -S --noconfirm gcc-libs
+arm-gnx5-linux-gnueabi-pacman -r $PWD/image -S --noconfirm acl
+arm-gnx5-linux-gnueabi-pacman -r $PWD/image -S --noconfirm expat
+arm-gnx5-linux-gnueabi-pacman -r $PWD/image -S --noconfirm lzo
+arm-gnx5-linux-gnueabi-pacman -r $PWD/image -S --noconfirm bzip2
+arm-gnx5-linux-gnueabi-pacman -r $PWD/image -S --noconfirm dropbear
+arm-gnx5-linux-gnueabi-pacman -r $PWD/image -S --noconfirm nano
 
-rm -fvR $IMG/usr/include
-rm -fvR $IMG/usr/share/iana-etc
-rm -fvR $IMG/usr/share/i18n
-rm -fvR $IMG/usr/share/doc
-rm -fvR $IMG/usr/share/info
-rm -fvR $IMG/usr/share/locale
-rm -fvR $IMG/usr/share/zoneinfo
-rm -fvR $IMG/usr/share/perl5
-rm -fvR $IMG/usr/share/man
-rm -fvR $IMG/usr/lib/*.a
-rm -fvR $IMG/usr/lib/*.la
-rm -fvR $IMG/usr/lib/audit
-rm -fvR $IMG/usr/lib/gconv
-rm -fvR $IMG/usr/lib/getconf
-rm -fvR $IMG/usr/lib/perl5
-rm -fvR $IMG/var/lib/pacman
+rm -fR $IMG/usr/include
+rm -fR $IMG/usr/share/iana-etc
+rm -fR $IMG/usr/share/i18n
+rm -fR $IMG/usr/share/doc
+rm -fR $IMG/usr/share/info
+rm -fR $IMG/usr/share/locale
+rm -fR $IMG/usr/share/zoneinfo
+rm -fR $IMG/usr/share/perl5
+rm -fR $IMG/usr/share/man
+rm -fR $IMG/usr/lib/*.a
+rm -fR $IMG/usr/lib/*.la
+rm -fR $IMG/usr/lib/audit
+rm -fR $IMG/usr/lib/gconv
+rm -fR $IMG/usr/lib/getconf
+rm -fR $IMG/usr/lib/perl5
+rm -fR $IMG/var/lib/pacman
 
 arm-gnx5-linux-gnueabi-strip $IMG/usr/lib/*.so
 arm-gnx5-linux-gnueabi-strip $IMG/usr/bin/*
 
-mkdir $IMG/var/lib/pacman
-
-cp ./passwd $IMG/etc/passwd
-cp ./resolv.conf $IMG/etc/resolv.conf
+install -d -m755 $IMG/var/lib/pacman
+install -d -m755 -o 1000 -g 100 $IMG/home/default
+install -D -m644 ./passwd  $IMG/etc/passwd
+install -D -m644 ./resolv.conf  $IMG/etc/resolv.conf
+install -d -m755 $IMG/mnt/gnx
 
 cd $IMG
 mkfs.jffs2 --little-endian --eraseblock=0x10000 -n --pad -o ../rescue.pacman.jffs2
